8b8c2f4b3b311a2eeb042a16e7a4f0e0d13f54cd
Milk-Cool <coder.tomsk@gmail.com>
2025-04-23 21:57:15 +0300

    include editor in build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { resolve } from 'path' 
 
 export default defineConfig({
     plugins: [
         ViteMinifyPlugin({}),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(import.meta.dirname, "index.html"),
+                editor: resolve(import.meta.dirname, "editor.html")
+            }
+        }
+    }
 })
